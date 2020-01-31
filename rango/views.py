987adac2d3e2550cb607820910e21d468a74e922
Bers <243997b@student.gla.ac.uk>
2020-01-31 16:57:47 +0000

    chapter 3 done

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Rango says here is the about page")
+
+def about(request):
+    return HttpResponse: ("Rango says here is the about page kappa")
